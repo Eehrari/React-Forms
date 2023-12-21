f43974e6a39df817c66b218197f8fcc78bc0a965
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2023-12-21 12:36:38 +0430

    KeyStrokeValidation

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,14 +8,13 @@ export default function Login() {
     password: '',
   });
 
+  const emailIsInvalid = !enteredValues.email.includes('@');
+  // const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
 
     console.log(enteredValues);
-    // setEnteredValues({
-    //   email: '',
-    //   password: ''
-    // })
   }
 
   function handleInputChange(identifier, value) {
@@ -47,6 +46,9 @@ export default function Login() {
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
