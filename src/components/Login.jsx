d1a05d7bddaba5d079a163e0b0465cb9d595c105
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2023-12-21 11:40:55 +0430

    Third Commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,16 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 
 export default function Login() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
-  // const [enteredValues, setEnteredValues] = useState({
-  //   email: '',
-  //   password: '',
-  // });
-
-
-
-  // function handleInputChange(identifier, value) {
-  //   setEnteredValues((prevValues) => ({
-  //     ...prevValues,
-  //     [identifier]: value,
-  //   }));
-  // }
-
-  function handleEmailChange(event) {
-    setEnteredEmail(event.target.value);
-  }
-
-  function handlePasswordChange(event) {
-    setEnteredPassword(event.target.value);
-  }
-
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    // console.log(enteredValues);
-    console.log("User Email: " + enteredEmail);
-    console.log("User Password: " + enteredPassword)
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    console.log(enteredEmail, enteredPassword);
   }
 
   return (
@@ -41,42 +20,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          {/* <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={(event) => handleInputChange('email', event.target.value)}
-            value={enteredValues.email}
-          /> */}
-
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={handleEmailChange}
-            value={enteredEmail}
-          />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          {/* <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-            value={enteredValues.password}
-          /> */}
-
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={handlePasswordChange}
-            value={enteredPassword}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
