1785df6f9200bd13cac0d04b080c8c314c668850
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2023-12-21 11:34:45 +0430

    Second Commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  // const [enteredValues, setEnteredValues] = useState({
+  //   email: '',
+  //   password: '',
+  // });
+
+
+
+  // function handleInputChange(identifier, value) {
+  //   setEnteredValues((prevValues) => ({
+  //     ...prevValues,
+  //     [identifier]: value,
+  //   }));
+  // }
+
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setEnteredPassword(event.target.value);
+  }
+
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('Submitted!');
+
+    // console.log(enteredValues);
+    console.log("User Email: " + enteredEmail);
+    console.log("User Password: " + enteredPassword)
   }
 
   return (
@@ -10,22 +40,49 @@ export default function Login() {
 
       <div className="control-row">
         <div className="control no-margin">
-          {/* <label for="email">Email</label> */}
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          {/* <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          /> */}
+
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+            value={enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          {/* <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+            value={enteredValues.password}
+          /> */}
+
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+            value={enteredPassword}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        {/* <button className="button" onSubmit={handleSubmit}> Login </button> */}
-        {/* <button type="button" className="button" onSubmit={handleSubmit}> Login </button> */}
-        <button className="button"> Login </button>
+        <button className="button">Login</button>
       </p>
     </form>
   );
