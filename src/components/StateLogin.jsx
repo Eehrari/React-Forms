8b4f4a3715a150e0798239d88dd300ec84893a4b
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2023-12-23 10:19:34 +0430

    Outsourcing Validation

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import Input from './Input.jsx';
+import { isEmail, isNotEmpty, hasMinLength } from '../util/validation.js';
 
 export default function Login() {
   // const [enteredEmail, setEnteredEmail] = useState('');
@@ -15,9 +16,12 @@ export default function Login() {
     password: false,
   });
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+  const emailIsInvalid =
+    didEdit.email &&
+    !isEmail(enteredValues.email) &&
+    !isNotEmpty(enteredValues.email);
   const passwordIsInvalid =
-    didEdit.password && enteredValues.password.trim().length < 6;
+    didEdit.password && !hasMinLength(enteredValues.password, 6);
 
   function handleSubmit(event) {
     event.preventDefault();
